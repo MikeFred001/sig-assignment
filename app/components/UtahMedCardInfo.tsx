4dd2f6b142b66d2d2c8c6057ad88c74b18344541
Michael Frederickson <mpfplus@gmail.com>
2025-05-09 22:30:33 -0600

    Utah Medical Card Information section is now responsive on various screen sizes.

--- app/components/UtahMedCardInfo.tsx
+++ app/components/UtahMedCardInfo.tsx
@@ -3,17 +3,26 @@ import Typography from "./common/Typography";
 
 export default function UtahMedCardInfo() {
   return (
-    <section className="flex justify-between gap-12">
+    <section className="flex flex-col lg:flex-row justify-between gap-12">
       <Image
         src="/assets/Consultation@2x.png"
         alt="Patient or client consulting with a medical professional"
-        width={550}
-        height={550}
-        className="object-cover"
+        width={300}
+        height={300}
+        className="object-cover w-full lg:w-auto hidden lg:block"
       />
-      <article className="">
-        <Typography className="text-[3rem]">Utah Medical Cards</Typography>
-        <Typography className="text-grey2 leading-[2.25rem]">
+      <article>
+        <Typography className="text-[2rem] text-center lg:text-start lg:text-[3rem]">
+          Utah Medical Cards
+        </Typography>
+        <Image
+          src="/assets/Consultation@2x.png"
+          alt="Patient or client consulting with a medical professional"
+          width={300}
+          height={300}
+          className="object-cover w-full lg:w-auto lg:hidden block"
+        />
+        <Typography className="text-grey2 leading-normal px-1 mt-2 lg:mt-0 lg:px-0 lg:leading-[2.25rem]">
           A medical cannabis card, also known as a medical marijuana card, is a
           state-issued identification card that allows qualified patients to
           legally purchase and use medical cannabis for the treatment of
