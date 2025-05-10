95063c135800f78f5a9220b83e8d3f01dabc67d1
Michael Frederickson <mpfplus@gmail.com>
2025-05-09 20:56:15 -0600

    Made header elements and main section wrapper much more responsive for all screen sizes.

--- app/page.tsx
+++ app/page.tsx
@@ -14,11 +14,11 @@ export default function Home() {
   return (
     <div>
       <Header />
-      <div className="h-[570px] bg-green1" />
-      <Typography className="text-center text-[1.8rem] text-green1 py-1 font-normal">
+      <div className="h-[400px] lg:h-[570px] bg-green1" />
+      <Typography className="text-center text-[1.25rem] lg:text-[1.8rem] text-green1 py-1 font-normal">
         Utah&apos;s Cannabis Community
       </Typography>
-      <main className="flex flex-col gap-6 px-[10rem] -mb-5">
+      <main className="flex flex-col gap-6 px-1 -mb-5 lg:px-[10rem] md:px-[6rem]">
         <UpcomingEvents />
         <ThingsToKnow />
         <BannerAd src="/assets/MockBannerAd@2x.png" />
